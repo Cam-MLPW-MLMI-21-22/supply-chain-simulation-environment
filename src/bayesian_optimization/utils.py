713e6dea150c5127083b33b7938c3300af824eae
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-01-22 19:24:01 +0000

    Initial SA work

--- src/bayesian_optimization/utils.py
+++ src/bayesian_optimization/utils.py
@@ -14,12 +14,16 @@ def plot_3d_boundary(X, Y, mesh_X, mesh_Y,
                      dpi=100,
                      plot_ci=True,
                      title=None,
-                     save_fig_path=None):
+                     save_fig_path=None,
+                     fig=None,
+                     ax=None,):
 
-    fig = plt.figure(figsize=(10, 10), dpi=dpi)
-    ax = fig.gca(projection='3d')
+    if not fig or not ax:
+        fig = plt.figure(figsize=(10, 10), dpi=dpi)
+        ax = fig.gca(projection='3d')
+
     surf = ax.plot_surface(mesh_X, mesh_Y, mu_plot.reshape(
-        (1000, 21)), cmap='viridis', linewidth=0, antialiased=False, alpha=0.60)
+        mesh_X.shape), cmap='viridis', linewidth=0, antialiased=False, alpha=0.60)
 
     if plot_ci:
         surf_var = ax.plot_surface(mesh_X, mesh_Y, (mu_plot-var_plot).reshape(
@@ -54,6 +58,54 @@ def plot_3d_boundary(X, Y, mesh_X, mesh_Y,
     ax.view_init(elev=elev, azim=angle)
     return fig, ax
 
+
+def plot_2d_boundary(X, Y, mesh_X, mesh_Y,
+                     mu_plot, var_plot,
+                     plot_new=False,
+                     new_X=None,
+                     new_Y=None,
+                     dpi=100,
+                     plot_ci=True,
+                     title=None,
+                     save_fig_path=None,
+                     fig=None,
+                     ax=None,
+                     plot_func='plot_surface',
+                     plot_kwargs={'linewidth': 0, 'antialiased': False}):
+
+    if not fig or not ax:
+        fig = plt.figure(figsize=(10, 10), dpi=dpi)
+        ax = fig.gca(projection='3d')
+
+    surf = getattr(ax, plot_func)(mesh_X, mesh_Y, mu_plot.reshape(
+        mesh_X.shape), cmap='viridis', alpha=0.50, **plot_kwargs)
+
+    if plot_ci:
+        surf_var = getattr(ax, plot_func)(mesh_X, mesh_Y, (mu_plot-var_plot).reshape(
+            (1000, 21)), cmap='viridis', alpha=0.20, **plot_kwargs)
+        surf_var = getattr(ax, plot_func)(mesh_X, mesh_Y, (mu_plot+var_plot).reshape(
+            (1000, 21)), cmap='viridis', alpha=0.20, **plot_kwargs)
+
+    ax.scatter(X[:, 0].flatten(), X[:, 1].flatten(), s=100,
+               marker="o", color="b", label="Initial observations")
+
+    if plot_new:
+        ax.scatter(new_X[:, 0].flatten(), new_X[:, 1].flatten(),
+                   s=100, marker="x", color="r", label="All observations")
+
+    ax.grid(True)
+    ax.set_xlabel("Num batteries")
+    ax.set_ylabel("Battery capacity")
+
+    if title:
+        plt.title(title)
+    plt.legend(loc='upper right', prop={'size': 15})
+
+    if save_fig_path:
+        for theta in range(0, 360, 10):
+            plt.savefig(save_fig_path+"_{}.png".format(theta))
+
+    return fig, ax
 
 def plot_3d_observed_rewards(X, Y,
                              elev,
